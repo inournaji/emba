48be6965615ac5b4d20853d0d7645175e0c5d361
Nour <nour_aldeen_44217@svuonline.org>
2017-07-30 18:19:10 +0300

    eventos css`

--- sites/all/themes/embavenebootstrap/templates/html.tpl.php
+++ sites/all/themes/embavenebootstrap/templates/html.tpl.php
@@ -62,12 +62,8 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-  <!-- HTML5 element support for IE6-8 -->
-  <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+
   <?php print $scripts; ?>
-  <script src="//code.tidio.co/rghxg82swjfaoowumpu5vfark3qcthvd.js"></script>
 </head>
 <body<?php print $body_attributes; ?>>
 <div class="spinner">
@@ -81,5 +77,10 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+<script src="//code.tidio.co/rghxg82swjfaoowumpu5vfark3qcthvd.js"></script>
+<!-- HTML5 element support for IE6-8 -->
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 </body>
 </html>
